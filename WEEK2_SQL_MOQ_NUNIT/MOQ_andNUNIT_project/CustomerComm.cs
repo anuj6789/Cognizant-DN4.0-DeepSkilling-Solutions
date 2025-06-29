ceb217c4a9e6aecafafc61bfd86418567a4a254f
anuj6789 <anuj.kumar.08022005@gmail.com>
2025-06-29 22:55:15 +0530

    Add files via upload

---
+++ WEEK2_SQL_MOQ_NUNIT/MOQ_andNUNIT_project/CustomerComm.cs
@@ -0,0 +1,17 @@
+namespace MoqAssignment
+{
+    public class CustomerComm
+    {
+        private readonly IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            return _mailSender.SendMail("cust123@abc.com", "Some Message");
+        }
+    }
+}
